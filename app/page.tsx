9631c0508cf80ac7a61d8b5094f17e01576286e8
azariahSILAS <azziesilas@gmail.com>
2025-12-22 15:38:20 -0800

    I just had to comment out some things on the root

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
-import Hero from "@/components/sections/Hero";
+// import Hero from "@/components/sections/Hero";
 import WhatsNew from "@/components/sections/WhatsNew";
 import LatestPosts from "@/components/sections/LatestPosts";
-import Topics from "@/components/sections/Topics";
-import Progress from "@/components/sections/Progress";
+// import Topics from "@/components/sections/Topics";
+// import Progress from "@/components/sections/Progress";
 import EmailCTA from "@/components/sections/EmailCTA";
 
 export default function HomePage() {
